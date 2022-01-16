dc9094d8d95041338c30ba3098fa0d6cd70ce10c
parthsujalshah <parthsujalshah@gmail.com>
2022-01-16 20:30:04 +0530

    test setup done

--- src/main/java/io/first/springbootstarter/topic/TopicController.java
+++ src/main/java/io/first/springbootstarter/topic/TopicController.java
@@ -1,17 +1,37 @@
 package io.first.springbootstarter.topic;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.Arrays;
 import java.util.List;
 
 @RestController // return type is converted to json
 public class TopicController {
+
+    @Autowired
+    private TopicService topicService;
+
     @RequestMapping("/topics")
     public List<Topic> getAllTopics() {
-        return Arrays.asList(
-                new Topic("spring", "Spring Framework", "Spring Framework Description"),
-                new Topic("java", "Core Java", "Core Java Description"),
-                new Topic("javascript", "JavaScript", "JavaScript Description")
-        );
+        return topicService.getAllTopics();
+    }
+
+
+    @RequestMapping("/topics/{id}")
+    public Topic getTopic(@PathVariable String id) {
+        return topicService.getTopic(id);
+    }
+
+    @RequestMapping(method=RequestMethod.POST, value="/topics")
+    public void addTopic(@RequestBody Topic topic) { topicService.addTopic(topic); }
+
+    @RequestMapping(method=RequestMethod.PUT, value="/topics/{id}")
+    public void updateTopic(@RequestBody Topic topic, @PathVariable String id) {
+        topicService.updateTopic(id, topic);
+    }
+
+    @RequestMapping(method=RequestMethod.DELETE, value="/topics/{id}")
+    public void deleteTopic(@PathVariable String id) {
+        topicService.deleteTopic(id);
     }
 }
